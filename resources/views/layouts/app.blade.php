95713fd91a6ee9dae905085b72c5bda5aa40fbc8
Djomar Lopulissa <166709731+djomarl@users.noreply.github.com>
2025-06-23 12:36:55 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple</title>
-    @vite(['resources/sass/app.scss','resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     @include('inc.navbar')
